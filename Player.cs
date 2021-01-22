b95c329007181f86970a3b863b999476403eacd0
Bhuvana-mani <74017862+Bhuvana-mani@users.noreply.github.com>
2021-01-22 18:19:08 +0100

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGameLibrary
+{
+     public class Player
+    {
+        public string Name { get; }
+        public GameScore GameScore { get; set; }
+        public Player(string name)
+        {
+            Name = name;
+            GameScore = new GameScore();
+        }
+    }
+}
